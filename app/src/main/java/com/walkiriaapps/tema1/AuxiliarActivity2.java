2af1d79c803a54b6ecb2279503ecbb6f576c8ed0
vicksWalkiria <vicks630@gmail.com>
2020-06-29 17:41:46 +0200

    fix onResult use and delete manifest watch feature

---
+++ app/src/main/java/com/walkiriaapps/tema1/AuxiliarActivity2.java
@@ -0,0 +1,67 @@
+package com.walkiriaapps.tema1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuxiliarActivity2 extends AppCompatActivity {
+
+    private Button button, buttonExit;
+    private TextView textLabel;
+    private int index = 100;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        Log.d("CURSO_ANDROID", "ESTOY EN ONCREATE");
+        button = findViewById(R.id.button);
+        textLabel = findViewById(R.id.label);
+        textLabel.setText(index + "");
+
+        button.setText("Clícame Auxiliar 2");
+
+        buttonExit = findViewById(R.id.button_exit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                index--; //index += 1; index = index+1;
+                textLabel.setText(index + "");
+            }
+        });
+
+        buttonExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("result", index);
+                setResult(Activity.RESULT_OK, returnIntent);
+
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/walkiriaapps/tema1/MainActivity.java
+++ app/src/main/java/com/walkiriaapps/tema1/MainActivity.java
@@ -13,15 +13,21 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private int ACTIVITY_TWO = 1;
+    private int ACTIVITY_THREE =2;
     private Button button;
     private TextView textLabel;
+    private Button button2;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == ACTIVITY_TWO && resultCode == Activity.RESULT_OK)
         {
-            textLabel.setText("Has clicado " + data.getIntExtra("result", 0) +" veces");
+            textLabel.setText("Has clicado " + data.getIntExtra("result", -1) +" veces");
+        }
+        if(requestCode == ACTIVITY_THREE && resultCode == Activity.RESULT_OK)
+        {
+            textLabel.setText("Aux. 2. El resultado es: " + data.getIntExtra("result", -1) +" veces");
         }
     }
 
@@ -32,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.button);
         textLabel = findViewById(R.id.label);
+        button2 = findViewById(R.id.button2);
+
+
 
         button.setText("Ir al contador");
 
@@ -40,6 +49,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, AuxiliarActivity.class);
                 startActivityForResult(i, ACTIVITY_TWO);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AuxiliarActivity2.class);
+                startActivityForResult(i, ACTIVITY_THREE);
             }
         });
     }
